13da348108441fe77d8a0ea36e17d65de0dc4d7f
CodeDad <github email address>
2018-04-04 16:01:22 -0400

    moved app.js and app.css to a containers folder, deleted react logo file

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -7,11 +6,11 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          {/* <img src={} className="" alt="" /> */}
+          <h1 className="App-title">Welcome to the Tab Factory</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+         Let make a tab!!!
         </p>
       </div>
     );
