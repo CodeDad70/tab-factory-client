6b9486324e50d399037b309e37d6fdd44775746f
CodeDad <github email address>
2018-04-07 13:57:42 -0400

    created Root.js file to handle the Provider and the Router

--- src/index.js
+++ src/index.js
@@ -4,15 +4,28 @@ import {Provider} from 'react-redux';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import store from './store.js';
 
-console.log(store)
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Home!</h1>
+    </div>
+  );
+};
 
 ReactDOM.render(
-  
+  <Router>
   <Provider store={store}>
+    
+    
     <App/>
+      <Route path="/" render={Home} />
+   
   </Provider>,   
+  </Router>,
 document.getElementById('root')
 );
 
