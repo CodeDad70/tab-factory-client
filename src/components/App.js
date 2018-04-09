298b761d97d498163194e25a0ec38e0d2f92d39b
CodeDad <github email address>
2018-04-09 12:26:57 -0400

    changed Song.js to SongsPage.js to fit convention for routing. Fixed all current imports and routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import Songs from '../containers/Songs'
+import Songs from '../containers/SongsPage'
 import Songform from '../containers/Songform';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 import '../stylesheets/App.css';
 
 class App extends Component {
@@ -9,32 +10,32 @@ class App extends Component {
   render() {
     return (
       <Router>
-      <div>
+        <div>
 
-        <div className = "songtab">
-          Test
-        </div>
-        
-        <div className = "Logo">
-          <h1> The Tab Factory 
-          <img src = "gear-logo-icon-92970.png" width="30px" height = "30px" alt="Gear Logo" className = "App-logo"/></h1><br/>
-        </div>
-        
-        <div className="Sidebar">    
-          {/* <img src={} className="" alt="" /> */} 
-          <h3> Select a Song : </h3>       
-          <Songs/>
-         
+          <div className = "songtab">
+            Test
+          </div>
           
-        </div>
+          <div className = "Logo">
+            <h1> The Tab Factory </h1><br/>
+          </div>
+          
+          <div className="Sidebar">    
+            {/* <img src={} className="" alt="" /> */} 
+            <h3> Select a Song : </h3>       
+            <Songs/>
+          
+            
+          </div>
 
-        <div className = "New-song">
-          <h3> Create a new tab: </h3>
-          <Songform/>
-        </div>
-        
+          <div className = "New-song">
+            <h3> Create a new tab: </h3>
+            <Songform/>
+            
+          </div>
+          
 
-      </div>  
+        </div>  
       </Router>
     );
   }
