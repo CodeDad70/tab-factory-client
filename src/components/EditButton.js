c566a8e427e414147750dfd12d4780bb4354c8ec
CodeDad <github email address>
2018-04-26 14:38:33 -0400

    created an EditButton component

---
+++ src/components/EditButton.js
@@ -0,0 +1,51 @@
+import React from 'react';
+import { Redirect } from 'react-router'
+import { connect } from 'react-redux';
+import {currentSong} from '../actions/song';
+
+class EditButton extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      fireRedirect: false,
+   }
+  }
+
+
+ 
+  handleOnClick = () => {
+   
+    this.props.currentSong(this.props.song);
+    this.setState({ fireRedirect: true })
+  }
+  
+
+
+  render() {
+    const { fireRedirect } = this.state
+
+
+    return (
+      <div>
+
+
+        <button onClick={this.handleOnClick}> Edit Song </button>
+        
+        {fireRedirect && (
+          
+          <Redirect to={'/'} />
+        )}
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    songs: state.songs,
+    song: state.song
+  }
+}
+
+export default connect(mapStateToProps, {currentSong})(EditButton);
