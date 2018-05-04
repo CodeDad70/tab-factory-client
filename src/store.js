9c9167f624e04e58918804b9986da2e73415ce93
CodeDad <github email address>
2018-05-04 12:19:03 -0400

    removed lyric reducers and imports from the store

--- src/store.js
+++ src/store.js
@@ -8,18 +8,11 @@ import thunk from 'redux-thunk';
 import songs from './reducers/songs';
 import song from './reducers/song';
 import songFormData from './reducers/songFormData';
-import lyrics from './reducers/lyrics';
-import lyricFormData from './reducers/lyricFormData';
-import lyric from './reducers/lyric';
 import songEditData from './reducers/songEditData';
-import lyricEditData from './reducers/lyricEditData';
+
 
 
 const reducers = combineReducers({
-  lyrics,
-  lyricFormData,
-  lyric,
-  lyricEditData,
   songs,
   songFormData,
   song, 
