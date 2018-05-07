6cb3c014e57d70d64a1d3746b8a160be4a998324
CodeDad <github email address>
2018-05-07 15:14:48 -0400

    created a presentational component - Nav thatrenders the contents of the NavBar. Removed all html from the NavBar container and now it calls up the Nav stateless component

--- src/containers/NavBar.js
+++ src/containers/NavBar.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
 import {getSongs} from '../actions/songs';
-import SongList from '../components/SongList';
+import Nav from '../components/Nav';
 
 class NavBar extends Component {
   
@@ -11,27 +10,10 @@ class NavBar extends Component {
   }
   
   render() {
-    const songs = this.props.songs;
-    const logo = "gear-logo-icon-92970.png"
-    
     return (
       <div>
-        <div className = "Logo">
-        <Link className="logo-link-style" to={"/"}>
-         <h1>The Tab Factory
-          <img src={`/${logo}`} width="30px" height = "30px" alt="Gear Logo" className = "App-logo"/>
-          </h1>
-          </Link>
-          
-        </div>
 
-        <div className= "Sidebar">
-        <br/>
-        <Link  className="link-style" to={`/new`}><h3>Create a new Tab</h3></Link>
-        <h5> or </h5>
-          <h4> Select a song: </h4>
-          <SongList songs={songs}/>
-        </div>
+       <Nav songs={this.props.songs}/>
         
       </div>    
     );   
