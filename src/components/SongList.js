147e9a0c05ec9f3e4dc7bd93e9cb8817a58f932f
CodeDad <github email address>
2018-05-10 10:43:02 -0400

    added a sort function to the songList component to allow sorting of the songs by alpha or number of likes etc

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -3,7 +3,32 @@ import { Link } from 'react-router-dom';
 
 const SongList = ({ songs }) => {
 
-  const renderSongs = songs.map(song =>
+  function compareValues(key, order='asc') {
+    return function(a, b) {
+      if(!a.hasOwnProperty(key) || 
+         !b.hasOwnProperty(key)) {
+        return 0; 
+      }
+      
+      const varA = (typeof a[key] === 'string') ? 
+        a[key].toUpperCase() : a[key];
+      const varB = (typeof b[key] === 'string') ? 
+        b[key].toUpperCase() : b[key];
+        
+      let comparison = 0;
+      if (varA > varB) {
+        comparison = 1;
+      } else if (varA < varB) {
+        comparison = -1;
+      }
+      return (
+        (order == 'desc') ? 
+        (comparison * -1) : comparison
+      );
+    };
+  }
+  
+  const renderSongs = songs.sort(compareValues('name', 'asc')).map(song =>
     <Link className="link-style" key={song.id} to={`/songs/${song.id}`}><h5><i>{song.name}</i> by {song.artist} </h5></Link>
   );
 
--- src/containers/WelcomePage.js
+++ src/containers/WelcomePage.js
@@ -4,39 +4,36 @@ import WelcomeCards from '../components/WelcomeCards';
 import { Link } from 'react-router-dom';
 import { updateCounter } from '../actions/songs';
 
-
 class WelcomePage extends React.Component {
 
-  handleOnClick(song){   
-    console.log(song.id)
+  handleOnClick(song) {
     const counterUpdate = Object.assign({}, song, {
       counter: song.counter + 1, id: song.id
     })
-    this.props.updateCounter(counterUpdate)   
+    this.props.updateCounter(counterUpdate)
   }
 
   render() {
-    const showAll = this.props.songs.map (song =>          
-      <div className = "songtab" >
-        <div className="song-card">       
-        <div className="song-card-title">
-          <h2><i>{song.name}</i> by {song.artist}</h2>
+
+    const showAll = this.props.songs.map(song =>
+      <div className="songtab" >
+        <div className="song-card">
+          <div className="song-card-title">
+            <h2><i>{song.name}</i> by {song.artist}</h2>
+          </div>
+
+          <div className="song-card-lyrics">
+            <Link className="link-style" key={song.id} to={`/songs/${song.id}`}><h5><i>See lyrics</i></h5></Link>
+            <br />
+
+            <button onClick={() => this.handleOnClick(song)} className="btn btn-primary"> Like This Song </button>
+
+            <br /><br />
+
+            <h4> This song has {song.counter} likes </h4>
+          </div >
         </div>
-
-        <div className="song-card-lyrics">
-          <Link className="link-style" key={song.id} to={`/songs/${song.id}`}><h5><i>See lyrics</i></h5></Link>
-          <br/>
-         
-          
-          <button onClick={() => this.handleOnClick(song)} className="btn btn-primary"> Like This Song </button>
-
-          <br/><br/>
-         
-          <h4> This song has {song.counter} likes </h4>
-        </div >
-
-      </div>
-        <br/><br/>
+        <br /><br />
       </div>
     )
 
@@ -51,9 +48,7 @@ class WelcomePage extends React.Component {
 const mapStateToProps = (state) => {
   return {
     songs: state.songs,
-  }; 
+  };
 };
 
-
-
-export default connect(mapStateToProps, {updateCounter})(WelcomePage);
+export default connect(mapStateToProps, { updateCounter })(WelcomePage);
