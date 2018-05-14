667e1e9acbc063a07fd051adf29890a5fa333cac
CodeDad <github email address>
2018-05-13 21:33:46 -0400

    refactored DELETE_SONG reducer and got rid of uneeded variable assignment "

--- src/reducers/songs.js
+++ src/reducers/songs.js
@@ -2,20 +2,12 @@ export default (state = [], action) => {
 
   switch (action.type) {
 
-    case 'EMPTY_SONG':
-      return action.song = [];
-
     case 'GET_SONGS':
       return action.songs;
 
-    case 'CURRENT_COUNT':
-      return action.song;
-
     case 'DELETE_SONG':
-      const song =  state.filter(song => song.id !== action.song);
-      state = song
-      return state
-
+      return state.filter(song => song.id !== action.song);
+     
     case 'UPDATE_SONG_COUNTER':
       return state.map(song => {
         if (song.id === action.song.id) {
