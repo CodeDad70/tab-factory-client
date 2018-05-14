4dea6bef83d3783e740c8189ee8d62ea02849070
CodeDad <github email address>
2018-05-13 22:13:39 -0400

    final clean code clean up

--- src/containers/WelcomePage.js
+++ src/containers/WelcomePage.js
@@ -2,37 +2,40 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { updateCounter } from '../actions/songs';
-import {SongSorter} from '../components/SongSorter';
+import { SongSorter } from '../components/SongSorter';
 
 class WelcomePage extends React.Component {
 
   handleOnClick(song) {
     const counterUpdate = Object.assign({}, song, {
-      counter: song.counter + 1})
+      counter: song.counter + 1
+    })
     this.props.updateCounter(counterUpdate)
   }
-
 
   render() {
 
     const showAll = this.props.songs.sort(SongSorter('count', 'desc')).map(song =>
       <div className="songtab" >
         <div className="song-card">
+          
           <div className="song-card-title">
             <h2><i>{song.name}</i> by {song.artist}</h2>
           </div>
 
           <div className="song-card-lyrics">
             <Link className="link-style" key={song.id} to={`/songs/${song.id}`}><h5><i>See lyrics</i></h5></Link>
-            <br/>
+            <br />
 
-            <button onClick={() => this.handleOnClick(song)} className="btn btn-primary"> Like This Song </button>
-            <br/><br/>
+            <button onClick={() => this.handleOnClick(song)} className="btn btn-primary"> 
+              Like This Song </button>
+            <br /><br />
 
             <h4> This song has {song.counter} likes </h4>
           </div >
+       
         </div>
-        <br/><br/>
+          <br /><br />
       </div>
     )
 
